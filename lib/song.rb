1b4823d694d9e59bbe48f52dbe2a458249c21ad5
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-10-11 23:08:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,9 +28,9 @@ class Song
     @@all
   end
   
-  def self.find_by_name(name)
-    all.detect{ |x| x.name == name }
-  end
+  # def self.find_by_name(name)
+  #   all.detect{ |x| x.name == name }
+  # end
 
   def self.find_or_create_by_name(name)
     find_by_name(name) || create(name)
