693cf13793cb72e1d02e9628527e0be19d1e4e0d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-10-11 22:44:30 +0000

    automatically backed up by learn

--- lib/concerns/persistable.rb
+++ lib/concerns/persistable.rb
@@ -2,7 +2,8 @@ module Persistable
   
   module ClassMethods
     def self.extended(base)
-      base.class_instance_variable_set(:@@all,)
+      base.class_instance_variable_set(:@@all,[])
+    end
     
     def reset_all
       self.all.clear
